cc6ba747c951b7247f399142b384a9fe9769b75d
Camilo Moncada <monk-da@hotmail.com>
2020-07-18 19:09:14 -0500

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - crceate and insert node in binary tree
+ * @binary_tree_t: is a binary tree
+ * @value: value the node
+ * Return - binary tree
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	
+}
