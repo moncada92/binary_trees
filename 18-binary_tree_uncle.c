eaa3c044e135e64309d342992a5936f075cd2f4e
Camilo Moncada <monk-da@hotmail.com>
2020-07-24 17:21:33 -0500

    response question 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,4 +13,22 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	return (binary_tree_sibling(node->parent));
 }
 
+/**
+ * binary_tree_sibling - fin the sibling node
+ * @node: node for find your sibling
+ * Return: value the node sibling
+ */
 
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *s;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		s = node->parent->right;
+	else
+		s = node->parent->left;
+	return (s);
+}
