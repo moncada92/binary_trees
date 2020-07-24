c1bf7c5d467187f2517a418272236adb4be5f8eb
Camilo Moncada <monk-da@hotmail.com>
2020-07-24 16:57:50 -0500

    hotfix 2 questio 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,22 +8,13 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t l = 0;
+	int leaves = 0;
 
 	if (tree == NULL)
 		return (0);
-	
-      	if (tree->left == NULL && tree->right == NULL)
-       		l++;
-       	if (tree->left != NULL)
-       		l++;
-       	if (tree->right != NULL)
-       		l++;
 
-       	binary_tree_leaves(tree->left);
-       	binary_tree_leaves(tree->right);
-	
-
-	return (l);
-
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
 }
